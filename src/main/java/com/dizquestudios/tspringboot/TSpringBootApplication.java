68e71af8202ac248fbde18ae314d6dab6b61b12e
sebastian_cardona3 <sebastian.co.dev@gmail.com>
2021-02-13 10:10:35 -0500

    Netbeans project configured // Proyecto de Netbeans configurado.

---
+++ src/main/java/com/dizquestudios/tspringboot/TSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.dizquestudios.tspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dizquestudios/tspringboot/TSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dizquestudios.tspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
